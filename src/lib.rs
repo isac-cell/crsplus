5daff92b7bd78a56ac4b3f3d6d56eed961ce244e
Isac Cell <isac-cell@protonmail.com>
2021-10-12 22:36:45 -0300

    add hi

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,14 @@ fn hello() -> PyResult<String> {
     Ok(("Hello").to_string())
 }
 
+#[pyfunction]
+fn hi(name: &str) -> PyResult<String> {
+    Ok(("Hi ".to_owned() + name).to_string())
+}
 #[pymodule]
 fn crsplus(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello, m)?)?;
+    m.add_function(wrap_pyfunction!(hi, m)?)?;
 
     Ok(())
 }
