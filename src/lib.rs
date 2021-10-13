d4c1147f55c4a1f728c6e8e1bc774e1fc7928ac0
Isac Cell <isac-cell@protonmail.com>
2021-10-13 11:27:33 -0300

    Add version, fix tests, more tests, and doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
 use pyo3::prelude::*;
 
+/// Says "Hello".
 #[pyfunction]
 fn hello() -> PyResult<String> {
     Ok(("Hello").to_string())
 }
 
+/// Says "Hi, [name]".
 #[pyfunction]
 fn hi(name: &str) -> PyResult<String> {
     Ok(("Hi ".to_owned() + name).to_string())
 }
+
 #[pymodule]
 fn crsplus(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+
     m.add_function(wrap_pyfunction!(hello, m)?)?;
     m.add_function(wrap_pyfunction!(hi, m)?)?;
 
--- tests/test_crsplus.py
+++ tests/test_crsplus.py
@@ -1,9 +1,21 @@
 import unittest
 
-from crsplus import hello
+from crsplus import hello, hi, __version__
 
-def test_hello():
-    assert hello() == 'Hello'
+
+class TestCRSPlus(unittest.TestCase):
+
+    def test_version(self):
+        self.assertEqual(__version__, '0.2.1')
+
+    def test_hello(self):
+        self.assertEqual(hello(), 'Hello')
+
+    def test_hi_when_name_is_Python_then_Hi_Python(self):
+        self.assertEqual(hi('Python'), 'Hi Python')
+
+    def test_hi_when_name_is_Rust_then_Hi_Rust(self):
+        self.assertEqual(hi('Rust'), 'Hi Rust')
 
 
 if __name__ == '__main__':
